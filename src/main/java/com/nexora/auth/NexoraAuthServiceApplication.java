5bcde5abd2c981737c35972d164483a7a798426c
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-02-23 18:59:46 +0300

    feat: configure auth service infrastructure and database connection

--- src/main/java/com/nexora/auth/NexoraAuthServiceApplication.java
+++ src/main/java/com/nexora/auth/NexoraAuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.nexora.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NexoraAuthServiceApplication {
 
 	public static void main(String[] args) {
