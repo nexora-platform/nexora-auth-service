8f7194ec94fd0d26a7cc97424dd3820b7a04c0eb
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-02-23 17:51:09 +0300

    chore: initialize auth service with base dependencies

---
+++ src/main/java/com/nexora/auth/NexoraAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexora.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexora/auth/NexoraAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexora.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
