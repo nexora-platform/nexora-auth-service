bbc1c9bb8468c681eb41a0d5d0566dffe71b7420
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-02-24 14:06:24 +0300

    feat: implement login and jwt token generation

---
+++ src/main/java/com/nexora/auth/security/JwtService.java
@@ -0,0 +1,44 @@
+package com.nexora.auth.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET = "nexora-super-secret-key-nexora-super-secret-key";
+    private final long EXPIRATION = 1000 * 60 * 60; // 1 hour
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
