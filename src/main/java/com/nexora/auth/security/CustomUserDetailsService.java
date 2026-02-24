42530ff6b0daa42fdfb555ba16673a8a3297c9ae
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-02-24 17:44:30 +0300

    feat(auth): implement custom user details service for database authentication

---
+++ src/main/java/com/nexora/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.nexora.auth.security;
+
+import com.nexora.auth.entity.User;
+import com.nexora.auth.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
