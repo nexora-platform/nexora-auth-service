69bd405fae5565c2ba0e2493a42aa66c8d45ee5d
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-06 20:18:57 +0300

    test: add Testcontainers PostgreSQL for integration tests

--- src/test/java/com/nexora/auth/NexoraAuthServiceApplicationTests.java
+++ src/test/java/com/nexora/auth/NexoraAuthServiceApplicationTests.java
@@ -2,9 +2,11 @@ package com.nexora.auth;
 
 import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Disabled("Disabled until test configuration is prepared")
 @SpringBootTest
+@Import(TestPostgresContainer.class)
 class NexoraAuthServiceApplicationTests {
 
 }
